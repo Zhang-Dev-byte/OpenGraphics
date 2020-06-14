b4983f5962aaa89067f52063d069efdc10bbdecf
Zhang-dev-byte <ozhanghe@gmail.com>
2020-06-14 15:38:21 -0700

    Application API Added

--- OpenGraphics/src/WinMain.cpp
+++ OpenGraphics/src/WinMain.cpp
@@ -1,7 +1,5 @@
 
-#include "Window.h"
-#include "WindowsMessageMap.h"
-#include <sstream>
+#include "App.h"
 
 
 int CALLBACK WinMain(
@@ -10,41 +8,5 @@ int CALLBACK WinMain(
 	LPSTR     lpCmdLine,
 	int       nCmdShow )
 {
-	try
-	{
-		Window wnd( 800,800,"Lol" );
-
-		MSG msg;
-		BOOL gResult;
-		while ((gResult = GetMessage(&msg, nullptr, 0, 0)) > 0)
-		{
-			// TranslateMessage will post auxilliary WM_CHAR messages from key msgs
-			TranslateMessage(&msg);
-			DispatchMessage(&msg);
-
-
-		}
-
-		// check if GetMessage call itself borked
-		if (gResult == -1)
-		{
-			return -1;
-		}
-
-		// wParam here is the value passed to PostQuitMessage
-		return msg.wParam;
-	}
-	catch( const WException& e )
-	{
-		MessageBox( nullptr,e.what(),e.GetType(),MB_OK | MB_ICONEXCLAMATION );
-	}
-	catch( const std::exception& e )
-	{
-		MessageBox( nullptr,e.what(),"Standard Exception",MB_OK | MB_ICONEXCLAMATION );
-	}
-	catch( ... )
-	{
-		MessageBox( nullptr,"No details available","Unknown Exception",MB_OK | MB_ICONEXCLAMATION );
-	}
-	return -1;
+	return App{}.Run();
 }
